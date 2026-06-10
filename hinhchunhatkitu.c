a6e728d0c54ef753bb6217ee1a074910390b6616
tu0729059-ship-it <tu0729059@gmail.com>
2026-06-10 16:02:05 +0700

    Create hinhchunhatkitu.c

---
+++ hinhchunhatkitu.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int m, n;
+    scan("%d %d", &m, &n);
+    char goc_dong_bang = '@' + n - 1;
+    for (int i = 1; i <= m; i++)
+    {
+        int da_in = 0;
+        char chu_in = '@' + i - 1;
+        while (chu_in <= goc_dong_bang && da_in < n)
+        {
+            printf("%c", chu_in);
+            da_in++;
+            chu_in++;
+        }
+        while (da_in < n)
+        {
+            printf("%c", goc_dong_bang);
+            da_in++;
+        }
+        printf("\n");
+    }
+}
