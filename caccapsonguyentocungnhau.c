2d7763a9341f1a9c3326d6b18ad2ce761631f1de
tu0729059-ship-it <tu0729059@gmail.com>
2026-06-13 09:06:04 +0700

    Create caccapsonguyentocungnhau.c

---
+++ caccapsonguyentocungnhau.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int ucln(int a, int b)
+{
+    while (b != 0)
+    {
+        int temp = a % b;
+        a = b;
+        b = temp;
+    }
+    return a;
+}
+
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    for (int i = a; i < b; i++)
+    {
+        for (int j = i + 1; j <= b; j++)
+        {
+            if (ucln(i, j) == 1)
+            {
+                printf("(%d,%d)\n", i, j);
+            }
+        }
+    }
+    return 0;
+}
